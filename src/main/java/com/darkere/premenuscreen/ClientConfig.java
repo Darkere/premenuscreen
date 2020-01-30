1c2ac2d75d48f1e167449df74488b3ef4be953d9
Darkere <Dark-277@web.de>
2020-01-30 23:33:12 +0100

    add link button

--- src/main/java/com/darkere/premenuscreen/ClientConfig.java
+++ src/main/java/com/darkere/premenuscreen/ClientConfig.java
@@ -9,12 +9,19 @@ public class ClientConfig {
     ForgeConfigSpec.ConfigValue<String> header;
     ForgeConfigSpec.ConfigValue<String> body;
     ForgeConfigSpec.ConfigValue<String> button;
+    ForgeConfigSpec.ConfigValue<String> link;
+
+
+
+    ForgeConfigSpec.ConfigValue<String> linkButtonText;
 
     public ClientConfig() {
         enable = builder.comment("Should the screen appear on the next Start?").define("shouldappear",false);
         header = builder.comment("The Header of the Message").define("header","Put the Header here");
         body = builder.comment("The Body of the Message").define("body","Put the Body of the Message here. Use \n for newline");
         button = builder.comment("The Text on the Button").define("button","Button Text here");
+        link = builder.comment("Optional: Adds a second button with a link that can be opened. Leave blank to disable the button").define("link","https://www.curseforge.com/minecraft/mc-mods/pre-menu-screen");
+        linkButtonText = builder.comment("Text on the Link Button").define("linkButtonText","CurseForge Page");
         spec = builder.build();
     }
 
@@ -34,6 +41,14 @@ public class ClientConfig {
     public String getButtonText(){
         return button.get();
     }
+    public String getLink() {
+        return link.get();
+    }
+
+    public String getLinkButtonText() {
+        return linkButtonText.get();
+    }
+
 
     public ForgeConfigSpec getSpec() {
         return spec;
--- src/main/java/com/darkere/premenuscreen/PreMenuConfirmScreen.java
+++ src/main/java/com/darkere/premenuscreen/PreMenuConfirmScreen.java
@@ -1,23 +1,55 @@
 package com.darkere.premenuscreen;
 
 import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
+import net.minecraft.client.gui.screen.ConfirmOpenLinkScreen;
 import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.Util;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 public class PreMenuConfirmScreen extends ConfirmScreen {
     Runnable run;
     StringTextComponent stringTextComponent = new StringTextComponent(PreMenuScreen.CLIENT_CONFIG.getBody());
-
-    public PreMenuConfirmScreen(BooleanConsumer s,Runnable run) {
-        super(s,new StringTextComponent(PreMenuScreen.CLIENT_CONFIG.getHeader()),new StringTextComponent("message2?"));
+    StringTextComponent link = new StringTextComponent(PreMenuScreen.CLIENT_CONFIG.getLink());
+    StringTextComponent linktext = new StringTextComponent(PreMenuScreen.CLIENT_CONFIG.getLinkButtonText());
+    URI uri;
+    public PreMenuConfirmScreen(BooleanConsumer s, Runnable run) {
+        super(s, new StringTextComponent(PreMenuScreen.CLIENT_CONFIG.getHeader()), new StringTextComponent("message2?"));
         this.run = run;
     }
 
     @Override
     protected void init() {
-        this.addButton(new Button(this.width/2 - 100,200,200,20,PreMenuScreen.CLIENT_CONFIG.getButtonText(),(x)-> run.run()));
+        if (link.getText().equals("")) {
+            this.addButton(new Button(this.width / 2 - 100, this.height - 40, 200, 20, PreMenuScreen.CLIENT_CONFIG.getButtonText(), (x) -> run.run()));
+        } else {
+            this.addButton(new ExtendedButton(this.width / 2 - 175, this.height - 40, 150, 20, linktext.getText(),this::openLinkScreen));
+            this.addButton(new ExtendedButton(this.width / 2 + 25, this.height - 40, 150, 20, PreMenuScreen.CLIENT_CONFIG.getButtonText(), (a)-> run.run()));
+        }
         this.listLines.clear();
         this.listLines.addAll(this.font.listFormattedStringToWidth(this.stringTextComponent.getFormattedText(), this.width - 50));
+    }
+
+
+    private void openLinkScreen(Button b){
+        try {
+            uri = new URI(link.getText());
+        } catch (URISyntaxException e) {
+            link = new StringTextComponent("broken link");
+            e.printStackTrace();
+        }
+        this.minecraft.displayGuiScreen(new ConfirmOpenLinkScreen((s) -> {
+            if (s) {
+                Util.getOSType().openURI(uri);
+            } else {
+                this.minecraft.displayGuiScreen(this);
+            }
+        }, link.getText(), false));
+
     }
 }
